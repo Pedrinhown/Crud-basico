2c541dd5c297c32233b0968b4fc72aaf8776c411
Pedro Henrique <pedrinhr03@gmail.com>
2021-09-19 21:59:11 -0300

    Initial
    initial

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtParcelas = new System.Windows.Forms.TextBox();
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.txtIdConta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(12, 42);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(123, 23);
+            this.txtData.TabIndex = 8;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(166, 42);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(123, 23);
+            this.txtValor.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Valor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Descrição";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(312, 42);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(123, 23);
+            this.txtDescricao.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(454, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Parcelas";
+            // 
+            // txtParcelas
+            // 
+            this.txtParcelas.Location = new System.Drawing.Point(454, 42);
+            this.txtParcelas.Name = "txtParcelas";
+            this.txtParcelas.Size = new System.Drawing.Size(123, 23);
+            this.txtParcelas.TabIndex = 14;
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.Location = new System.Drawing.Point(621, 40);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(86, 24);
+            this.btnTestar.TabIndex = 16;
+            this.btnTestar.Text = "Gravar";
+            this.btnTestar.UseVisualStyleBackColor = true;
+            this.btnTestar.Click += new System.EventHandler(this.btnTestar_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 97);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(565, 150);
+            this.dataGridView2.TabIndex = 17;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(284, 310);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(86, 24);
+            this.btnEditar.TabIndex = 18;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(183, 310);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(86, 24);
+            this.btnExcluir.TabIndex = 19;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // txtIdConta
+            // 
+            this.txtIdConta.Location = new System.Drawing.Point(12, 312);
+            this.txtIdConta.Name = "txtIdConta";
+            this.txtIdConta.Size = new System.Drawing.Size(123, 23);
+            this.txtIdConta.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 15);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Id de Conta";
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Location = new System.Drawing.Point(392, 312);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(86, 24);
+            this.btnCarregar.TabIndex = 22;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnListar.Location = new System.Drawing.Point(621, 97);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(86, 24);
+            this.btnListar.TabIndex = 23;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = false;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 354);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtIdConta);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btnTestar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtParcelas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtParcelas;
+        private System.Windows.Forms.Button btnTestar;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.TextBox txtIdConta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const string connectionString = "server=192.168.79.128; user id=root; password=ph1234; database=descomplicaFinanceiro";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Excluir()
+        {
+            try
+            {
+                int intRetorno = 0;
+                if (txtIdConta.Text == string.Empty)
+                {
+                    throw new Exception("Informe o id da conta que deseja excluir");
+                }
+
+                MySqlConnection mCon = new MySqlConnection(connectionString);
+                mCon.Open();
+
+                StringBuilder sql = new StringBuilder();
+
+                sql.Append("delete from contas");
+                sql.Append($" where id={txtIdConta.Text}");
+
+                MySqlCommand mCmd = new MySqlCommand(sql.ToString(), mCon);
+                intRetorno = mCmd.ExecuteNonQuery();
+
+                if (intRetorno == 1)
+                {
+                    MessageBox.Show("Excluiu");
+                    Listar();
+                    LimparTela();
+                }
+                else
+                {
+                    MessageBox.Show("Não excluiu");
+                }
+                sql.Clear();
+                mCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Listar()
+        {
+            try
+            {
+                MySqlConnection mCon = new MySqlConnection(connectionString);
+                mCon.Open();
+
+                StringBuilder sql = new StringBuilder();
+
+                sql.Append("select * from contas");
+
+                MySqlCommand mCmd = new MySqlCommand(sql.ToString(), mCon);
+                MySqlDataAdapter mAdp = new MySqlDataAdapter(mCmd);
+
+                DataSet ds = new DataSet();
+                mAdp.Fill(ds);
+
+                dataGridView2.DataSource = ds.Tables[0];
+                dataGridView2.Refresh();
+
+                sql.Clear();
+                mCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Editar()
+        {
+            try
+            {
+                if (txtIdConta.Text == string.Empty)
+                {
+                    throw new Exception("Informe o id da conta que deseja editar");
+                }
+
+                int intRetorno = 0;
+                MySqlConnection mCon = new MySqlConnection(connectionString);
+                mCon.Open();
+
+                StringBuilder sql = new StringBuilder();
+                sql.Append("update contas");
+                sql.Append($" set descricao = '{txtDescricao.Text}',");
+                sql.Append($" dataConta = '{txtData.Text}',");
+                sql.Append($" parcelas = '{txtParcelas.Text}',");
+                sql.Append($" valor = '{decimal.Parse(txtValor.Text)}'");
+                sql.Append($" where id = '{int.Parse(txtIdConta.Text)}';");
+
+                MySqlCommand mCmd = new MySqlCommand(sql.ToString(), mCon);
+                intRetorno = mCmd.ExecuteNonQuery();
+
+                if (intRetorno == 1)
+                {
+                    MessageBox.Show("Editou");
+                    Listar();
+                    LimparTela();
+                }
+                else
+                {
+                    MessageBox.Show("Não editou");
+                }
+                sql.Clear();
+                mCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Carregar()
+        {
+            try
+            {
+                if (txtIdConta.Text == string.Empty)
+                {
+                    throw new Exception("Informe o id da conta que deseja carregar");
+                }
+
+                MySqlConnection mCon = new MySqlConnection(connectionString);
+                mCon.Open();
+
+                StringBuilder sql = new StringBuilder();
+
+                sql.Append("select * from contas");
+                sql.Append($" where id={decimal.Parse(txtIdConta.Text)}");
+
+                MySqlCommand mCmd = new MySqlCommand(sql.ToString(), mCon);
+                MySqlDataAdapter mAdp = new MySqlDataAdapter(mCmd);
+
+                DataSet ds = new DataSet();
+                mAdp.Fill(ds);
+
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    DataRow rd = ds.Tables[0].Rows[0];
+                    txtData.Text = rd["dataConta"].ToString();
+                    txtDescricao.Text = rd["descricao"].ToString();
+                    txtIdConta.Text = rd["id"].ToString();
+                    txtParcelas.Text = rd["parcelas"].ToString();
+                    txtValor.Text = rd["valor"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Conta não encontrada.");
+                }
+                sql.Clear();
+                mCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Gravar()
+        {
+            try
+            {
+                int intRetorno = 0;
+                MySqlConnection mCon = new MySqlConnection(connectionString);
+                mCon.Open();
+
+                StringBuilder sql = new StringBuilder();
+
+                sql.Append("insert into contas(dataConta, descricao, parcelas, valor)");
+                sql.Append($" values('{txtData.Text}', '{txtDescricao.Text}',{int.Parse(txtParcelas.Text)}, {decimal.Parse(txtValor.Text)})");
+                MySqlCommand mCmd = new MySqlCommand(sql.ToString(), mCon);
+                intRetorno = mCmd.ExecuteNonQuery();
+
+                if (intRetorno == 1)
+                {
+                    MessageBox.Show("Gravou");
+                    Listar();
+                    LimparTela();
+                }
+                else
+                {
+                    MessageBox.Show("Não gravou");
+                }
+                sql.Clear();
+                mCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void LimparTela()
+        {
+            txtData.Text = string.Empty;
+            txtDescricao.Text = string.Empty;
+            txtIdConta.Text = string.Empty;
+            txtParcelas.Text = string.Empty;
+            txtValor.Text = string.Empty;
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+            Gravar();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            Editar();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            Excluir();
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            Carregar();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+    }
+}
